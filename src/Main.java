82f90dc101b35fa609f143b4588da6ca73b3af42
Phil Picinic <PhilPicinic@gmail.com>
2014-02-11 03:22:57 -0500

    first multithread app

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	
+	public static void main(String args[]){
+		new MyThread("First Thread");
+		new MyThread("Second Thread");
+		new MyThread("Third Thread");
+		new MyThread("Fourth Thread");
+		new MyThread("Fifth Thread");
+		new MyThread("Sixth Thread");
+		new MyThread("Seventh Thread");
+		new MyThread("Eight Thread");
+	}
+}
---
+++ src/MyThread.java
@@ -0,0 +1,30 @@
+
+public class MyThread implements Runnable{
+	Thread t;
+	
+	public MyThread(String name){
+		t = new Thread(this, name);
+		t.start();
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		int total = 0;
+		for(int i = 3; i <= 200000; i++){
+			boolean isprime = true;
+			for(int x = 2; x < i; x++){
+				if(i % x == 0){
+					isprime = false;
+					break;
+				}
+			}
+			if(isprime){
+				total++;
+//				System.out.println(t.getName() + " found prime number: " + i);
+			}
+		}
+		System.out.println(t.getName() + " found " + total + " prime numbers.");
+	}
+	
+
+}
